fdbf7c276914b61e7a0f2ff470edea8c5adb664b
Khadjiakbar Rustamovic <khadjiakbarbox@gmail.com>
2021-01-11 00:22:42 +0500

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './style.css';
+export class Footer extends Component {
+  render() {
+    return (
+      <footer>2020 Rustamovic</footer>
+    )
+  }
+}
+
+export default Footer;
